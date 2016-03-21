ca7c986d66ed2fddf4fbb7eb7d2532f4e5d62c0b
Tanvir Mahmud Emon <emontme@gmail.com>
2016-03-21 07:10:22 +0600

    Create CpbookSubeen_26.py

---
+++ CpbookSubeen_26.py
@@ -0,0 +1,22 @@
+# Accepted, Solved by Emon
+tc = input()
+for i in range(tc):
+    m = input()
+    if m >= 80 and m <= 100:
+        print "Case %r: A+"%(i+1)
+    elif m >=75 and m <= 79:
+        print "Case %r: A"%(i+1)
+    elif m >=70 and m <= 74:
+        print "Case %r: A-"%(i+1)
+    elif m >=65 and m <= 69:
+        print "Case %r: B+"%(i+1)
+    elif m >=60 and m <= 64:
+        print "Case %r: B"%(i+1)
+    elif m >=55 and m <= 59:
+        print "Case %r: B-"%(i+1)
+    elif m >=50 and m <= 54:
+        print "Case %r: C"%(i+1)
+    elif m >=45 and m <= 49:
+        print "Case %r: D"%(i+1)
+    else:
+        print "Case %r: F"%(i+1)
